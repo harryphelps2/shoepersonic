86c17ab1921aed70b2b5b4fa306f351fe739dc70
harryphelps2 <harryphelps1@gmail.com>
2018-10-03 08:23:49 +0100

    Import env.py if in virtual env

--- shoepersonic/settings.py
+++ shoepersonic/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import env
+
+if os.path.exists('env.py'):
+    import env
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
