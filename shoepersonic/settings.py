4dfe3b9cdc88b4d15324976a8bce2f3317f1d3d1
harryphelps2 <harryphelps1@gmail.com>
2018-10-25 08:28:51 +0100

    Add heroku to the allowed hosts

--- shoepersonic/settings.py
+++ shoepersonic/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['shoepersonic.herokuapp.com/']
+ALLOWED_HOSTS = ['shoepersonic.herokuapp.com']
 
 
 # Application definition
